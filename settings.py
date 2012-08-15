dc181897313bc5deaf669aa8e8c073805389d9d0
Arda Eren <ardaeren13@gmail.com>
2012-08-15 16:14:51 +0300

    fixed #1 fixed #2

--- settings.py
+++ settings.py
@@ -127,6 +127,7 @@ FACEBOOK_STORE_FRIENDS = True
 #FACEBOOK_LOGIN_DEFAULT_REDIRECT = '/facebook/connect/'
 
 
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
